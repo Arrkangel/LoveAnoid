387dfd79d98bcd32bd39d1549602c9335476df23
Arrkangel <hankbrobeck@gmail.com>
2014-11-01 00:20:47 -0700

    Progress!
    Ball moves, basic collision, etc.

--- actor.lua
+++ actor.lua
@@ -7,29 +7,102 @@ function newRect(xPos,yPos,xSize,ySize)
 	rect.y=yPos
 	rect.width=xSize
 	rect.height=ySize
-	rect.updateValues=function(self)
+	rect.updateValues=function(self) --call after moving the rect to put the colision helper values in the right spot
 		rect.left=self.x
 		rect.right=self.x+self.width
 		rect.top=self.y
 		rect.bottom=self.y+self.height
 	end
-	rect.updateValues()
+	rect:updateValues()
 	rect.copy=function(self)
 		return newRect(self.x,self.y,self.width,self.height)
 	end
+	return rect
 
 end
 
 
-function newActor(image,x,y,width,height,collides)
+function newActor(name,x,y,width,height,collides,r,g,b)
 	local act={}
-	act.image=image
+	act.name=name
+
 	act.collides=collides
 	act.rect=newRect(x,y,width,height)
+
+	local color={}
+	color.r=r
+	color.g=g
+	color.b=b
+	act.color=color
+
+	act.update=function(self,dt) end --blank function, to be replaced for any objects that need actual logic
+
+
 	act.id=actors.curID
 	actors[act.id]=act
 	actors.curID=actors.curID+1
 	return act
-
-
 end
+
+function drawActors()
+	for i,v in ipairs(actors) do
+		local color=v.color
+		local rect=v.rect
+		love.graphics.setColor(color.r,color.g,color.b)
+		love.graphics.rectangle("fill",rect.x,rect.y,rect.width,rect.height)
+	end
+end
+
+function updateActors(dt)
+	for i,v in ipairs(actors) do
+		v:update(dt)
+	end
+end
+
+function checkOverlap(a,b)
+	local r=a.left<=b.right and b.left<=a.right and a.top<=b.bottom and b.top<=a.bottom
+	return r
+end
+
+
+
+
+function collisionCheck(actor,x,y)
+	local coldata={} --table for storing colision data
+	coldata.actor=nil--actor collided with (nil for nothing)
+	coldata.norm=nil --normal vector the actor collided with (for ball physics)
+	coldata.horiz=true
+
+	local r1=actor.rect:copy() --copy actor rect and move to new position
+	r1.x=x
+	r1.y=y
+	r1:updateValues()
+
+	coldata.rect=r1 --rect created by copy, to optimize slightly
+
+	for i,v in ipairs(actors) do
+		if actor.id~=v.id and v.collides then
+			local norm={}
+			norm.x=0
+			norm.y=0
+			local r2=v.rect
+			
+			if checkOverlap(r1,r2) then
+				
+				if r2.right<actor.rect.left or r2.left> actor.rect.right then
+					coldata.horiz=false
+				end
+
+
+				coldata.actor=v
+				coldata.norm=norm
+				return coldata
+
+			end
+		end
+	end
+	return coldata
+end
+
+
+
--- main.lua
+++ main.lua
@@ -1,6 +1,88 @@
 require("actor")
+
+function love.load()
+	love.window.setTitle("LoveAnoid")
+	love.window.setMode(640,480)
+
+	paddle=newActor("paddle",300,400,100,20,true,255,0,0)
+
+	paddle.update=function(self,dt)
+		local a=0
+		if love.keyboard.isDown("a") then
+			a=1
+		end
+		local d=0
+		if love.keyboard.isDown("d") then
+			d=1
+		end
+		local multi=100
+		local move=(d-a)*multi*dt
+		self.rect.x=self.rect.x+move
+		self.rect:updateValues()
+	end
+
+
+	
+	ball=newActor("ball",320,300,10,10,true,255,255,255)
+	local vel={}
+	vel.x=100
+	vel.y=100
+	ball.vel=vel
+
+	ball.update=function(self,dt)
+		
+		local x=self.rect.x+self.vel.x*dt
+		local y=self.rect.y+self.vel.y*dt
+
+		local coldata=collisionCheck(self,x,y)
+		if coldata.actor~=nil then
+			local horiz=coldata.horiz
+			local nVel={}
+			nVel.x=self.vel.x
+			nVel.y=self.vel.y
+			if horiz then
+				nVel.y=nVel.y*-1
+			else
+				print("Hi")
+				nVel.x=nVel.x*-1
+			end
+			self.vel=nVel
+			print(nVel.x ..",".. nVel.y)
+			x=self.rect.x+self.vel.x*dt
+			y=self.rect.y+self.vel.y*dt
+			self.rect.x=x
+			self.rect.y=y
+			self.rect:updateValues()
+
+		else
+			self.rect.x=x
+			self.rect.y=y
+			self.rect:updateValues()
+		end
+	end
+
+	walltop=newActor("walltop",0,0,640,10,true,0,0,255)
+	wallleft=newActor("wallleft",0,0,10,480,true,0,0,255)
+	wallbottom=newActor("wallbottom",0,470,640,10,true,0,0,255)
+	wallright=newActor("wallright",630,0,10,480,true,0,0,255)
+
+
+
+
+
+end
+
+
+function love.update(dt)
+
+	updateActors(dt)
+
+
+end
+
+
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
-	paddle=newActor()
+	
+	drawActors()
 
 end
